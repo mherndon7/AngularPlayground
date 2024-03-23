637321558b1cee5f10bd4386e2f472e2d8a60099
Mitchell Herndon <mherndon2010@gmail.com>
2024-03-23 11:24:20 -0500

    Added getters for specific hero details

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input } from '@angular/core';
-import { Hero } from '../interfaces/hero';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule, Location } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from '../services/hero.service';
+import { Hero } from '../interfaces/hero';
 
 @Component({
   selector: 'app-hero-detail',
@@ -10,6 +12,25 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './hero-detail.component.html',
   styleUrl: './hero-detail.component.css',
 })
-export class HeroDetailComponent {
-  @Input() hero?: Hero;
+export class HeroDetailComponent implements OnInit {
+  hero?: Hero;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
