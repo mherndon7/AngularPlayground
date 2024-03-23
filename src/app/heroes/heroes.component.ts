c75bcceaf7696f0604b7c1ca5fe384b5b86ee08f
Mitchell Herndon <mherndon2010@gmail.com>
2024-03-23 10:05:14 -0500

    Added the hero interface

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Hero } from '../interfaces/hero';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.css'
+  styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
-
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm',
+  };
 }
