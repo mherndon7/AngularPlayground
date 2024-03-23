ef86191a51ca5da0c1c4c0dcbeec6914b7aa2c65
Mitchell Herndon <mherndon2010@gmail.com>
2024-03-23 10:54:36 -0500

    Added hero component routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'heroes',
+    loadComponent: () =>
+      import('./heroes/heroes.component').then((m) => m.HeroesComponent),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
