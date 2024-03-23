2e4b24eb6a19693dff1c75cb8087294b7a9ac606
Mitchell Herndon <mherndon2010@gmail.com>
2024-03-23 10:15:04 -0500

    Converted heroes component to standalone

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { Hero } from '../interfaces/hero';
 import { HEROES } from '../data/mock-heroes';
+import { FormsModule } from '@angular/forms';
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-heroes',
+  standalone: true,
+  imports: [FormsModule, NgIf, NgFor, UpperCasePipe],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })
